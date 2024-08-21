f3d1dcf262a34689551a19049fdb153338449d65
sultan ali <165585090+sultana-07@users.noreply.github.com>
2024-08-21 06:07:15 +0000

    learn variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1234
+let accountEmail = "sultanalih8@gmail.com"
+var accountPassword = "ssltan"
+accountCity = "jaipur"
+
+// accountId = 77777
+accountEmail = "irfan@gmail.com"
+accountPassword = "irfan"
+accountCity = "bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
